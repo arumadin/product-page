f175523ca3460fe6ee336eb5985b9fee569996cc
Arum <arum.adiningtyas@gmail.com>
2024-08-14 09:39:33 +0700

    [Product Detail] Update css

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -27,7 +27,7 @@ function Page({params} : {params: {productId:string}}) {
             .then((res) => res.json())
             .then((data) => {
                 let productDataByID = data.filter((item: ProductProps) => item.id === parseInt(params.productId) )[0]
-                setProduct(productDataByID)
+                setProduct({...productDataByID})
             })
     }, [])
 
@@ -36,48 +36,55 @@ function Page({params} : {params: {productId:string}}) {
         {product && (
             <div>
             <h2>Product {params.productId}</h2>
-            <div className={styles.productCol}>
-                <div className="product-carousel">
-                    <Image
-                        src={product.image}
-                        width={200}
-                        height={200}
-                        alt='Picture of '
-                        priority={false}
-                    />
-                    {product.images && (
-                        product.images.map((image: string, idx: number) => {
-                            return (
-                                <Image
-                                    key={idx}
-                                    src={image}
-                                    width={200}
-                                    height={200}
-                                    alt='Picture of '
-                                    priority={false}
-                                />
-                            )
-                        })
-                    )}
-                </div>
-                <div>
-                    <ProductGallery></ProductGallery>
-                </div>
-                <div className="review"></div>
-                    <div className="product-desc">
-                        <h2>{product.title}</h2>
-                        <div>Add to wishlist</div>
-                        <p>Price: $ {product.price}</p>
-                        <p>{product.description}</p>
-                        <div>Amount</div>
-                        <button>Add to Cart</button>
-                        <div className="accordion">
-                            <div className="product-details">
-                                {product.description}
-                            </div>
-                            <div className="delivery">Delivery and returns</div>
-                        </div>
+            <div className={styles.productWrap}>
+                <div className={styles.productPhoto}>
+                    <div className="product-carousel">
+                        <Image
+                            src={product.image}
+                            width={200}
+                            height={200}
+                            alt='Picture of '
+                            priority={false}
+                        />
+                        {product.images && (
+                            product.images.map((image: string, idx: number) => {
+                                return (
+                                    <Image
+                                        key={idx}
+                                        src={image}
+                                        width={200}
+                                        height={200}
+                                        alt='Picture of '
+                                        priority={false}
+                                    />
+                                )
+                            })
+                        )}
                     </div>
+                    <div>
+                        <ProductGallery></ProductGallery>
+                    </div>
+                </div>
+                <div className={styles.productInfo}>
+                    <h2>{product.title}</h2>
+                    <div>Add to wishlist</div>
+                    <p>Price: $ {product.price}</p>
+                    <p>{product.description}</p>
+                    <div>Amount</div>
+                    <button>Add to Cart</button>
+                    <div className="accordion">
+                        <div className="product-details">
+                            {product.description}
+                        </div>
+                        <div className="delivery">Delivery and returns</div>
+                    </div>
+                </div>
+                <div className={styles.productReview}>
+                    <h4>Review</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, repudiandae. Corporis dolorum ratione animi quisquam delectus molestias perferendis numquam sed.</p>
+
+                </div>
+                    
             </div>
             <div className="recommendations">
                 You may also like...
