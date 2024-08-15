98c84056e49bfaccfe63164f4d86e4cb594f59ba
Arum <arum.adiningtyas@gmail.com>
2024-08-15 12:24:53 +0700

    Add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../styles/Footer.module.scss'
+
+function Footer() {
+  return (
+    <footer className={styles.footerWrap}>
+        Village Grocer. 2024
+    </footer>
+  )
+}
+
+export default Footer
